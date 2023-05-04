0ecaa1ba7a99abda7aecfb34e937bab80e2d875c
khanhngoc52 <132522427+khanhngoc52@users.noreply.github.com>
2023-05-04 22:07:31 +0700

    Update rinne2.ps1

--- rinne2.ps1
+++ rinne2.ps1
@@ -5,7 +5,7 @@ choco feature enable -n allowGlobalConfirmation
 choco install chrome-remote-desktop-host 
 
 
-& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AbUR2VP3rXjycKijZTNIKAr9Lt_hPNNG12BC1mPo9XiaWv7wcAMoPqxo4xAk7BEdKqW4GQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME --pin=123123
+& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AbUR2VMsQjmvms0xt-kQ2zWyACl41XXrmAB2kqk7Qn9pzCttbkUHNeXXHNdgKA9gfC2jzA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME --pin=123123
 
 write-host Mã pin của bạn là: 123123
 
